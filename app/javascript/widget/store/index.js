950abe05138da90607f25236521d186168918848
MajesteitBart <bartvandermeeren@gmail.com>
2020-03-20 22:03:09 +0100

    no idea what im doing here

--- app/javascript/widget/store/index.js
+++ app/javascript/widget/store/index.js
@@ -3,7 +3,6 @@ import Vuex from 'vuex';
 import appConfig from 'widget/store/modules/appConfig';
 import contact from 'widget/store/modules/contact';
 import conversation from 'widget/store/modules/conversation';
-import mautic from 'widget/store/modules/mautic';
 import agent from 'widget/store/modules/agent';
 
 Vue.use(Vuex);
@@ -14,6 +13,5 @@ export default new Vuex.Store({
     contact,
     conversation,
     agent,
-    mautic,
   },
 });
