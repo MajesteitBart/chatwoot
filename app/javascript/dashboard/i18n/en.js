86e7d19f0e0f9c7f5418a100f70c18f8d6a08a22
MajesteitBart <bartvandermeeren@gmail.com>
2020-03-20 17:18:19 +0100

    Fixed language issue

--- app/javascript/dashboard/i18n/en.js
+++ app/javascript/dashboard/i18n/en.js
@@ -1,8 +1,7 @@
 import en from './locale/en';
-import nl from './locale/nl';
 
 export default {
-  ...nl,
+  ...en,
   APP_GLOBAL: {
     TRIAL_MESSAGE: 'days trial remaining.',
     TRAIL_BUTTON: 'Buy Now',
---
+++ app/javascript/dashboard/i18n/nl.js
@@ -0,0 +1,21 @@
+import nl from './locale/nl';
+
+export default {
+  ...nl,
+  APP_GLOBAL: {
+    TRIAL_MESSAGE: 'dagen van de trial resterend.',
+    TRAIL_BUTTON: 'Nu kopen',
+  },
+  COMPONENTS: {
+    CODE: {
+      BUTTON_TEXT: 'Kopie maken',
+      COPY_SUCCESSFUL: 'Gekopieerd naar klembord',
+    },
+  },
+  CONFIRM_EMAIL: 'Controleren...',
+  SETTINGS: {
+    INBOXES: {
+      NEW_INBOX: 'Inbox toevoegen',
+    },
+  },
+};
